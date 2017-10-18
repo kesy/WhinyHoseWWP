20459c2c479b326880592a7de3a454032fe3ecdd
Kathryn <Kathryn>
2017-10-17 22:30:20 -0700

    directions/walls not working

--- MyAI.py
+++ MyAI.py
@@ -29,6 +29,8 @@ class MyAI ( Agent ):
         self.position=(1,1) #(x,y)
         self.direction=0 #{0:L, 1:U, 2:R, 3:D}
         self.deadwumpus=False 
+        self.hasarrow=True
+        self.num_moves=0
 
         # ======================================================================
         # YOUR CODE ENDS
@@ -39,10 +41,21 @@ class MyAI ( Agent ):
         # YOUR CODE BEGINS
         # ======================================================================
         action=self.__actions [ random.randrange ( len ( self.__actions ) ) ]
+        #if self.position not in self.world:
         self.world[self.position]=[]
         self.observe(self.position, stench, breeze, glitter, bump, scream)
+        self.num_moves+=1
+        if 'g' in self.world[self.position]:
+            return Agent.Action.GRAB
+        if 's' in self.world[self.position] and self.hasarrow:
+            action = Agent.Action.SHOOT
+            self.hasarrow=False
+        if self.position == (1,1) and 'b' in self.world[self.position]:
+            action = Agent.Action.CLIMB  
+        
+        print(self.position, self.direction, action, self.num_moves, self.world)  
         self.move(self.position, action)
-        print(self.position, action, self.world)
+
         return action
         # ======================================================================
         # YOUR CODE ENDS
@@ -55,17 +68,14 @@ class MyAI ( Agent ):
         Agent.Action.TURN_LEFT,
         Agent.Action.TURN_RIGHT,
         Agent.Action.FORWARD,
-        Agent.Action.CLIMB,
-        Agent.Action.SHOOT,
-        Agent.Action.GRAB
     ]
     __directions = [0,1,2,3]
     def move(self, postion, action):
         if action==Agent.Action.TURN_LEFT:
-            self.direction = self.direction+1%4
+            self.direction = (self.direction+1)%4
         elif action==Agent.Action.TURN_RIGHT:
-            self.direction = abs(self.direction-1%4)
-        elif action==Agent.Action.FORWARD:
+            self.direction = 3-self.direction
+        elif action==Agent.Action.FORWARD and self.direction not in self.world[self.position]:
             if self.direction==0:
                 self.position=(self.position[0]+1,self.position[1])
             elif self.direction==1:
@@ -84,7 +94,14 @@ class MyAI ( Agent ):
         if glitter:
             self.world[position].append('g')
         if bump:
-            self.world[position].append('w')
+            if self.direction == 0 or self.direction == 1:
+                for key in self.world:
+                    if key[0]==position[0] and self.direction not in self.world[key]:
+                        self.world[key].append(self.direction)
+            else:
+                for key in self.world:
+                    if key[1]==position[1] and self.direction not in self.world[key]:
+                        self.world[key].append(self.direction)
         if scream:
             self.deadwumpus=True
     # ======================================================================
